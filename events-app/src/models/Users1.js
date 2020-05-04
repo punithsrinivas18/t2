81285d1a52e2e4dd312583d1350c134314b61ddc
Punith Srinivas <puniths181296@gmail.com>
2020-05-04 14:57:01 -0400

    commit

--- events-app/src/models/Users1.js
+++ events-app/src/models/Users1.js
@@ -1,21 +1,10 @@
-
-const Users1 = [
-    { Name: 'Punith', Password: '2020', Email: 'punith@newpaltz.edu',},
-    { Name: 'Bracha', Password: '5780', Email: 'chabad@newpaltz.edu',},
-    
-];
+import myFetch from "./myFetch";
 
 export let CurrentUser = null;
 
-export function signup(email, name, password) {
+export async function Login(email, name, password) {
 
-    const use = Users1.find(x => x.Email == email);
-    if(!use) throw Error('User not found');
+    const user = await myFetch('/users/login', { email, name, password }) ;
 
-    const u1 = Users1.find(x => x.Name == name);
-    if(!u1) throw Error('User not found');
-    if(use.Password != password) throw Error('Wrong Password');
-    
-
-    return CurrentUser = use;
+    return CurrentUser = user;
 }
